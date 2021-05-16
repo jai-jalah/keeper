13aa4c2a3aee9885381d40adcdca4c5d2846c122
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-16 16:42:01 +0100

    Add CreateArea component

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
+
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
