272cb03fbe4e07a6da8612c6b3354f77d046ea39
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-16 17:04:38 +0100

    Implement deleteNote functionality

--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -19,9 +19,13 @@ function CreateArea(props) {
 
   function submitNote(event) {
     props.onAdd(note);
-
+    setNote({
+      title: '',
+      content: '',
+    });
     event.preventDefault();
   }
+
   return (
     <div>
       <form>
